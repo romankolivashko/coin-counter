42b3f31143bd5a91ea15fe339890d7af823778ed
Roman Kolivashko <rkolivashko@gmail.com>
2021-11-08 15:24:24 -0800

    Passing 2nd test

--- __tests__/coin.test.js
+++ __tests__/coin.test.js
@@ -1,10 +1,17 @@
 import {coinCounter} from './../src/js/coin.js'
 
 describe('coinCounter',() => {
-
 test (' return number of quarters', () =>{
-expect(coinCounter(100)).toEqual(expect.stringContaining("Quarters: 8"));
+expect(coinCounter(100)).toEqual(expect.stringContaining("4 quarters"));
 
 })
 })
+
+describe('coinCounter',() => {
+test (' return number of quarters, dimes, nickels, and pennies', () =>{
+expect(coinCounter(136)).toEqual(expect.stringContaining("5 quarters 1 dimes 1 pennies"));
+
+})
+})
+
 
--- src/js/coin.js
+++ src/js/coin.js
@@ -1,45 +1,33 @@
-export const coinCounter=(num) => {
-if (isNaN(num)){
-return"Enter a number only."
+export const coinCounter = (num) => {
+  if (isNaN(num)) {
+    return "Enter a number only.";
+  }
+  if (num === 0) {
+    return "";
+  } else if (num / 25 >= 1) {
+    let quarters = Math.trunc(num / 25);
+    console.log(quarters);
+    return `${quarters} quarters ` + coinCounter(num - quarters * 25);
+  } else if (num / 10 >= 1) {
+    let dimes = Math.trunc(num / 10);
+    return `${dimes} dimes ` + coinCounter(num - dimes * 10);
+  } else if (num / 5 >= 1) {
+    let nickels = Math.trunc(num / 5);
+    return `${nickels} nickels ` + coinCounter(num - nickels);
+  } else {
+    let pennies = num;
+    return `${pennies} pennies ` + coinCounter(0);
+  }
 };
-if(num === 0){
-return"";
-
-}else if (num/25>=1){
-let quarters = Math.trunc(num/25);
-console.log(quarters);
-return `${quarters} quarters ` + coinCounter(num - quarters * 25);
-
-}else if (num/10>=1){
-let dimes = Math.trunc(num/10);
-return `${dimes} dimes ` + coinCounter(num - dimes * 10);
-
-}else if (num/5>=1){
-let nickels = Math.trunc(num/5);
-return `${nickels} nickels ` + coinCounter(num - nickels);
-
-}else{
-let pennies = num;
-return `${pennies} pennies ` + coinCounter(0);
-};
-}
-
-
-
-
-
-
-
 
 //pass $356.37
 // let dollars = Math.trunc(amount);
 //return 356
 // let penny = (amount - dollars) * 100;
-//356.37 - 356 = 0.37; 
+//356.37 - 356 = 0.37;
 //0.37 * 100 = 37
 
-// let nickel = penny % 5  
+// let nickel = penny % 5
 
 // if(cents === 0){
 // return cents};
-
